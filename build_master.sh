d0ff2deb88f6aa6a24513fa55bba6bc4ea191124
Park Ju Hyung <qkrwngud825@gmail.com>
2020-02-21 23:40:39 +0900

    build_master: prepare for AnyKernel3 release
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- build_master.sh
+++ build_master.sh
@@ -3,6 +3,7 @@
 if [[ "${1}" != "skip" ]] ; then
 	./build_clean.sh
 	./build_kernel.sh "$@" || exit 1
+	./build_recovery.sh skip || exit 1
 fi
 
 VERSION="$(cat version)-$(date +%F | sed s@-@@g)"
@@ -11,10 +12,9 @@ if [ -e boot.img ] ; then
 	rm arter97-kernel-$VERSION.zip 2>/dev/null
 	# cp boot.img arter97-kernel-$VERSION.img
 
-	# Pack AnyKernel2
+	# Pack AnyKernel3
 	rm -rf kernelzip
-	mkdir -p kernelzip/dtbs
-	cp arch/arm64/boot/Image.gz-dtb kernelzip/
+	mkdir kernelzip
 	echo "
 kernel.string=arter97 kernel $(cat version) @ xda-developers
 do.devicecheck=1
@@ -26,12 +26,19 @@ device.name2=OnePlus3T
 block=/dev/block/bootdevice/by-name/boot
 is_slot_device=0
 ramdisk_compression=gz
+supported.versions=9,9.0
 " > kernelzip/props
 	cp -rp ~/android/anykernel/* kernelzip/
 	cd kernelzip/
-	7z a -mx0 arter97-kernel-$VERSION-tmp.zip *
+	7z a -mx9 arter97-kernel-$VERSION-tmp.zip *
+	7z a -mx0 arter97-kernel-$VERSION-tmp.zip ../arch/arm64/boot/Image.gz-dtb
 	zipalign -v 4 arter97-kernel-$VERSION-tmp.zip ../arter97-kernel-$VERSION.zip
 	rm arter97-kernel-$VERSION-tmp.zip
 	cd ..
 	ls -al arter97-kernel-$VERSION.zip
+fi
+
+if [ -e recovery.img ] ; then
+	rm arter97-recovery-$VERSION.zip 2>/dev/null
+	cp recovery.img arter97-recovery-$VERSION.img
 fi
